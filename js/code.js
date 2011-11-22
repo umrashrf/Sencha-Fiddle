3850f06053ead5382d914eae33b89fa25f8b4a5f
Umair Ashraf <umr.ashrf@gmail.com>
2011-11-23 00:44:31 +0500

    prototype in progress

---
+++ js/code.js
@@ -0,0 +1,55 @@
+﻿function SenchaFiddle($$) {
+    this.init = function () {
+        var windowHeight = $$(window).height();
+        var headerHeight = $$(".headerOuterWrapper").height();
+        var footerHeight = $$(".footer").height();
+        var occupiedHeight = headerHeight + footerHeight;
+        var remainingHeight = windowHeight - occupiedHeight;
+        $$(".contentOuterWrapper").height(remainingHeight);
+
+        this.tabList = $$("#tabList");
+        this.tabListWidth = this.tabList.parent().width();
+    };
+
+    this.run = function () {
+    };
+
+    this.save = function () {
+    };
+
+    this.beutify = function () {
+    };
+
+    this.share = function () {
+    };
+
+    this.login = function () {
+    };
+
+    /*
+    type: MODEL, STORE, CONTROLLER
+    */
+    this.addFile = function (type) {
+
+    };
+
+    /*
+    filePath: the path from array
+    */
+    this.openFile = function (filePath) {
+    };
+
+    this.moveTabsLeft = function (e) {
+        if (e) e.preventDefault();
+
+        var left = this.tabList.css("left").replace("px", "");
+        this.tabList.css("left", (parseFloat(left) - this.tabListWidth) + "px");
+    };
+
+    this.moveTabsRight = function (e) {
+        if (e) e.preventDefault();
+
+        var left = this.tabList.css("left").replace("px", "");
+        this.tabList.css("left", (parseFloat(left) + this.tabListWidth) + "px");
+    };
+}
