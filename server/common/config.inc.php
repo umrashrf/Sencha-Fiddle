0fbbcc85b322ab77a0e32fdd2849de0c16fc42b5
Umair Ashraf <umr.ashrf@gmail.com>
2012-12-06 18:20:55 +0500

    deleted old files

--- server/common/config.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('DB_SERVER', 'localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-    define('DB_DATABASE', 'senchafiddle');
-	
-	define('BASE_URL', '/local/v11/');
-?>
